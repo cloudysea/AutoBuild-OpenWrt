19e4ab9a958ce568a119b6a6d9c1c665456011d3
cloudysea <35562113+cloudysea@users.noreply.github.com>
2019-12-17 16:40:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/192.168.20.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.20.254/g' openwrt/package/base-files/files/bin/config_generate
